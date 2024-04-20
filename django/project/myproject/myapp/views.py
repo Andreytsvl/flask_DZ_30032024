9648ac2d0f0e3adb9c77094a9ea3bb22d7dc08ed
andreytsvl15 <andreytsvl15@gmail.com>
2024-04-20 07:52:49 +0300

    Комит 1 для Джанго
    Signed-off-by: andreytsvl15 <andreytsvl15@gmail.com>

--- django/project/myproject/myapp/views.py
+++ django/project/myproject/myapp/views.py
@@ -32,12 +32,12 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    logger.info(f"Посетитель зашёл на главную страницу: {request.user}")
-    return HttpResponse("<h1>Добро пожаловать на страницу Иванова Ивана Иваныча</h1>"
-                        "<p>Описание главной страницы...</p>")
+    logger.info('''f"Посетитель зашёл на главную страницу: {request.user}")
+    return HttpResponse(<h1>Добро пожаловать на страницу Иванова Ивана Иваныча</h1>
+                        <p>Описание главной страницы...</p>''')
 
 
 def about(request):
     logger.info(f"Посетитель посетил страницу 'О себе': {request.user}")
-    return HttpResponse("<h1>О сайте</h1>"
-                        "<p>Информация о сайте Иванова Ивана Иваныча...</p>")
+    return HttpResponse('''<h1>О сайте</h1>
+                        <p>Информация о сайте Иванова Ивана Иваныча...</p>''')
